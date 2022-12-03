f36a9ee7a9f3c9f4fe68539912f59953cf9cdf5a
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-12-03 13:35:48 +0100

    start the practice

--- Clase3_Clases.playground/Contents.swift
+++ Clase3_Clases.playground/Contents.swift
@@ -10,26 +10,28 @@ class Heroe {
     init(){
         //self.name = name;
         //self.power = power;
+        name = ""
     }
     
     // SOBRECARGA DE CONSTRUCTORES
     
-    convenience init(name : String = "", power : Float = 0.0){
-        self.init()
-        
-        self.name = name
-        self.power = power
+        convenience init(name : String = "", power : Float = 0.0){
+            self.init()
+            
+            self.name = name
+            self.power = power
+        }
     }
-
     let goku = Heroe(name : "Goku", power : 99)
 
-    goku.name
-    goku.power
+    //goku.name
+    //goku.power
     
     goku.name = "Cell"
     goku.name
     
     var gokuHeroe = goku;
-}
+
+
 
 
